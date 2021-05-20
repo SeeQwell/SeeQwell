280d8dc78a4f889ff029dfbc04f3a2f847014f00
SeeQwell <76976833+SeeQwell@users.noreply.github.com>
2021-05-20 20:34:15 +0100

    Add files via upload

---
+++ counter/scripts/scriptchet.js
@@ -0,0 +1,3 @@
+ar.addEventListener('input', function () {
+aaaa.value = (this.value * 7.69230769230).toFixed(2);
+});     
---
+++ counter/scripts/scriptodin.js
@@ -0,0 +1,3 @@
+aq.addEventListener('input', function () {
+a.value = (this.value * 1.17647058823).toFixed(2);
+});
